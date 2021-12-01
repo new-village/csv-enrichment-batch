916458daa0968296882f5a2b4791dcb1c9c402c4
new-village <kazuki.niimura@gmail.com>
2021-12-01 07:00:29 +0000

    add set environment vals shell

---
+++ init.sh
@@ -0,0 +1,8 @@
+# Azure Portal > Storage Account > Access Key
+export STORAGE_ACCOUNT="YOUR ACCOUNT URI"
+export STORAGE_KEY1="YOUR ACCOUNT KEY"
+
+# Azure Portal > Container Registry > KEY
+export USERNAME="YOUR USER NAME"
+export PASSWORD="YOUR PASSWORD"
+export REGISTRY="YOUR REGISTRY NAME"
